82ab51085fa836075d46e0f19915e108debce10a
qErAzEd <pasikdavid123@gmail.com>
2025-06-20 18:52:07 +0300

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,10 @@
+import customtkinter
+from .gui import ClickerGUI
+
+def main():
+    root = customtkinter.CTk()
+    app = ClickerGUI(root)
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
